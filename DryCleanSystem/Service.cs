c5fc33d4b19adebfc8a740ccf99d141116241cba
Rowan <rowansherif182@gmail.com>
2025-09-19 02:26:06 +0300

    Add User class (abstract class) and service class

---
+++ DryCleanSystem/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DryCleanSystem
+{
+    public class Service
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+
+        public void dispalyService() {
+            Console.WriteLine($"Service ID: {id}");
+            Console.WriteLine($"Service Name: {name}");
+            Console.WriteLine($"Service Price: {price}");
+        }
+    }
+}
---
+++ DryCleanSystem/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DryCleanSystem
+{
+    public abstract class User
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string role { get; set; }
+
+        public User(int id, string userName, string password, string phone, string address, string role) {
+            this.id = id;
+            this.userName = userName;
+            this.password = password;
+            this.phone = phone;
+            this.address = address;
+            this.role = role;
+        }
+
+        abstract public void register(string userName, string password, string confirmPassword, string phone, string address, string role);
+        abstract public bool login(string userName, string password);
+        abstract public void logout();
+    }
+}
